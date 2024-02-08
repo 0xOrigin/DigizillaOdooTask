ff26a57cbcd237b1e115abc692da433faca71c8f
0xOrigin <egyahmed.ezzat120@gmail.com>
2024-02-08 19:24:25 +0200

    Add manifest file

---
+++ odoo/addons/digizilla/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name':'Digizilla',
+    'version': '1.0.0',
+    'author': '0xOrigin',
+    'summary': 'Assessment task for Python developer role.',
+    'sequence': 1,
+    'description':'This is an Assessment task for Python developer role at Digizilla Company.',
+    'category':'Customization',
+    'website':'https://github.com/0xOrigin',
+    'depends':['base', 'web', 'mail'],
+    'application': True,
+    'installable': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'report/report_template.xml',
+        'report/digizilla_report.xml',
+        'views/digizilla_view.xml',
+    ],
+    'qweb': [
+        'views/digizilla_view.xml', 
+    ]
+}
